a56d87be9f620068c601fddbc2692500eb6ae6d4
sbirvin1s <sbirvin1s@gmail.com>
2023-02-09 13:35:27 -0600

    adds UserContext

---
+++ contexts/UserContext.js
@@ -0,0 +1,55 @@
+/* ========== EXTERNAL MODULES ========== */
+import React, { useContext, useState } from 'react';
+
+
+/* ========== CONTEXTS ========== */
+
+const UserContext = React.createContext();
+
+/* ========== EXPORTS ========== */
+
+export function useUserInfo() {
+  return useContext(UserContext)
+}
+
+export function UserProvider({ children }) {
+
+  /* --- STATE HOOKS --- */
+  const [userInfo, setUserInfo] = useState();
+
+  /* --- EVENT HANDLERS --- */
+  const updateUserInfo = ({ target:{ name, value } }) => {
+    event.preventDefault();
+    setUserInfo(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const updateInfo = info => {
+    event.preventDefault();
+    setUserInfo(info);
+  }
+
+  const updateSpecificInfo = (name, value) => {
+    event.preventDefault();
+    setUserInfo(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  }
+
+  /* --- RENDERER --- */
+  return (
+    <UserContext.Provider
+      value={{
+        userInfo,
+        updateInfo,
+        updateUserInfo,
+        updateSpecificInfo,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,14 @@
 /* ========== INTERNAL MODULES ========== */
 import '@/styles/globals.css';
 import { AuthProvider } from 'contexts/AuthContext';
+import { UserProvider } from 'contexts/UserContext';
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     </AuthProvider>
   )
 }
